bfa1a87de54c3486454c5ed0fe3ba735a6f56411
97jayv122 <jay-v@live.fr>
2025-06-02 17:45:41 +0200

    Login Logout

---
+++ litrevu/authentication/views.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from . import forms
+
+def signup(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request,
+                  'authentication/signup.html', context={'form': form})
---
+++ litrevu/litrevu/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for litrevu project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import authentication.views
+import blog.views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,PasswordChangeDoneView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True),
+        name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('home/', blog.views.home, name='home'),
+    path('passwordchange/', PasswordChangeView.as_view(
+        template_name='authentication/registration.html',
+        success_url='passwordchanged'
+    ),
+         name='passwordchange'),
+    path('passwordchanged/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'
+    ),
+         name='passwordchanged'),
+    path('signup/',authentication.views.signup, name='signup'),
+]
